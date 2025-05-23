ae218d67200b8b1c64a0848aa63106227800a492
Andrew Bindraw <mrbindraw@gmail.com>
2025-05-23 23:14:21 +0300

    Refactoring: renamed ui objects

--- widget.cpp
+++ widget.cpp
@@ -9,21 +9,21 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    connect(ui->hSlider_Front, SIGNAL(valueChanged(int)), ui->lbl_VolFront, SLOT(setNum(int)));
-    //connect(ui->hSlider_Front, SIGNAL(valueChanged(int)), this, SLOT(handleVolumeValueChanged(int, ui->lbl_Front->text()))); // warning: Signature is not normalized. [clazy-connect-not-normalized]
-    connect(ui->hSlider_Front, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Front->value(), ui->lbl_Front->text()); });
+    connect(ui->hsldChannelFront, SIGNAL(valueChanged(int)), ui->lbVolumeFront, SLOT(setNum(int)));
+    //connect(ui->hsldChannelFront, SIGNAL(valueChanged(int)), this, SLOT(handleVolumeValueChanged(int, ui->lbChannelFront->text()))); // warning: Signature is not normalized. [clazy-connect-not-normalized]
+    connect(ui->hsldChannelFront, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hsldChannelFront->value(), ui->lbChannelFront->text()); });
 
-    connect(ui->hSlider_Rear, SIGNAL(valueChanged(int)), ui->lbl_VolRear, SLOT(setNum(int)));
-    connect(ui->hSlider_Rear, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Rear->value(), ui->lbl_Rear->text()); });
+    connect(ui->hsldChannelRear, SIGNAL(valueChanged(int)), ui->lbVolumeRear, SLOT(setNum(int)));
+    connect(ui->hsldChannelRear, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hsldChannelRear->value(), ui->lbChannelRear->text()); });
 
-    connect(ui->hSlider_Sub, SIGNAL(valueChanged(int)), ui->lbl_VolSub, SLOT(setNum(int)));
-    connect(ui->hSlider_Sub, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Sub->value(), ui->lbl_Sub->text()); });
+    connect(ui->hsldChannelSub, SIGNAL(valueChanged(int)), ui->lbVolumeSub, SLOT(setNum(int)));
+    connect(ui->hsldChannelSub, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hsldChannelSub->value(), ui->lbChannelSub->text()); });
 
-    connect(ui->hSlider_Center, SIGNAL(valueChanged(int)), ui->lbl_VolCenter, SLOT(setNum(int)));
-    connect(ui->hSlider_Center, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Center->value(), ui->lbl_Center->text()); });
+    connect(ui->hsldChannelCenter, SIGNAL(valueChanged(int)), ui->lbVolumeCenter, SLOT(setNum(int)));
+    connect(ui->hsldChannelCenter, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hsldChannelCenter->value(), ui->lbChannelCenter->text()); });
 
-    connect(ui->hSlider_Side, SIGNAL(valueChanged(int)), ui->lbl_VolSide, SLOT(setNum(int)));
-    connect(ui->hSlider_Side, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Side->value(), ui->lbl_Side->text()); });
+    connect(ui->hsldChannelSide, SIGNAL(valueChanged(int)), ui->lbVolumeSide, SLOT(setNum(int)));
+    connect(ui->hsldChannelSide, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hsldChannelSide->value(), ui->lbChannelSide->text()); });
 
     SysAudio::getInstance().init();
 }
@@ -31,24 +31,24 @@ Widget::Widget(QWidget *parent) :
 void Widget::showEvent(QShowEvent *)
 {
     int volumeFront = SysAudio::getInstance().getPartVolume("Front");
-    ui->hSlider_Front->setValue(volumeFront);
-    ui->lbl_VolFront->setText(QString::number(volumeFront));
+    ui->hsldChannelFront->setValue(volumeFront);
+    ui->lbVolumeFront->setText(QString::number(volumeFront));
 
     int volumeRear = SysAudio::getInstance().getPartVolume("Rear");
-    ui->hSlider_Rear->setValue(volumeRear);
-    ui->lbl_VolRear->setText(QString::number(volumeRear));
+    ui->hsldChannelRear->setValue(volumeRear);
+    ui->lbVolumeRear->setText(QString::number(volumeRear));
 
     int volumeSub = SysAudio::getInstance().getPartVolume("Subwoofer");
-    ui->hSlider_Sub->setValue(volumeSub);
-    ui->lbl_VolSub->setText(QString::number(volumeSub));
+    ui->hsldChannelSub->setValue(volumeSub);
+    ui->lbVolumeSub->setText(QString::number(volumeSub));
 
     int volumeCenter = SysAudio::getInstance().getPartVolume("Center");
-    ui->hSlider_Center->setValue(volumeCenter);
-    ui->lbl_VolCenter->setText(QString::number(volumeCenter));
+    ui->hsldChannelCenter->setValue(volumeCenter);
+    ui->lbVolumeCenter->setText(QString::number(volumeCenter));
 
     int volumeSide = SysAudio::getInstance().getPartVolume("Side");
-    ui->hSlider_Side->setValue(volumeSide);
-    ui->lbl_VolSide->setText(QString::number(volumeSide));
+    ui->hsldChannelSide->setValue(volumeSide);
+    ui->lbVolumeSide->setText(QString::number(volumeSide));
 
     // if volume = slider-value slot slider_valueChanged don't call!
     // this flag fix twice set volume on load app (when get-volume != slider-value)
