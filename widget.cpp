268ec0cc9bf8142e38819ee6983832481e2fa7ee
Andrew Bindraw <mrbindraw@gmail.com>
2025-05-23 20:50:07 +0300

    Setup only one slot for channel volume changed

--- widget.cpp
+++ widget.cpp
@@ -9,6 +9,22 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    connect(ui->hSlider_Front, SIGNAL(valueChanged(int)), ui->lbl_VolFront, SLOT(setNum(int)));
+    //connect(ui->hSlider_Front, SIGNAL(valueChanged(int)), this, SLOT(handleVolumeValueChanged(int, ui->lbl_Front->text()))); // warning: Signature is not normalized. [clazy-connect-not-normalized]
+    connect(ui->hSlider_Front, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Front->value(), ui->lbl_Front->text()); });
+
+    connect(ui->hSlider_Rear, SIGNAL(valueChanged(int)), ui->lbl_VolRear, SLOT(setNum(int)));
+    connect(ui->hSlider_Rear, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Rear->value(), ui->lbl_Rear->text()); });
+
+    connect(ui->hSlider_Sub, SIGNAL(valueChanged(int)), ui->lbl_VolSub, SLOT(setNum(int)));
+    connect(ui->hSlider_Sub, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Sub->value(), ui->lbl_Sub->text()); });
+
+    connect(ui->hSlider_Center, SIGNAL(valueChanged(int)), ui->lbl_VolCenter, SLOT(setNum(int)));
+    connect(ui->hSlider_Center, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Center->value(), ui->lbl_Center->text()); });
+
+    connect(ui->hSlider_Side, SIGNAL(valueChanged(int)), ui->lbl_VolSide, SLOT(setNum(int)));
+    connect(ui->hSlider_Side, &QSlider::valueChanged, this, [this](){ handleVolumeValueChanged(ui->hSlider_Side->value(), ui->lbl_Side->text()); });
+
     SysAudio::getInstance().init();
 }
 
@@ -39,7 +55,7 @@ void Widget::showEvent(QShowEvent *)
     _isAppLoading = false;
 }
 
-void Widget::on_hSlider_Front_valueChanged(int value)
+void Widget::handleVolumeValueChanged(int volume, const QString &channelName)
 {
     if(_isAppLoading)
     {
@@ -47,61 +63,7 @@ void Widget::on_hSlider_Front_valueChanged(int value)
     }
 
     qDebug() << Q_FUNC_INFO;
-
-    SysAudio::getInstance().setPartVolume("Front", value);
-    ui->lbl_VolFront->setText(QString::number(value));
-}
-
-void Widget::on_hSlider_Rear_valueChanged(int value)
-{
-    if(_isAppLoading)
-    {
-        return;
-    }
-
-    qDebug() << Q_FUNC_INFO;
-
-    SysAudio::getInstance().setPartVolume("Rear", value);
-    ui->lbl_VolRear->setText(QString::number(value));
-}
-
-void Widget::on_hSlider_Sub_valueChanged(int value)
-{
-    if(_isAppLoading)
-    {
-        return;   
-    }
-
-    qDebug() << Q_FUNC_INFO;
-
-    SysAudio::getInstance().setPartVolume("Subwoofer", value);
-    ui->lbl_VolSub->setText(QString::number(value));
-}
-
-void Widget::on_hSlider_Center_valueChanged(int value)
-{
-    if(_isAppLoading)
-    {
-        return;
-    }
-
-    qDebug() << Q_FUNC_INFO;
-
-    SysAudio::getInstance().setPartVolume("Center", value);
-    ui->lbl_VolCenter->setText(QString::number(value));
-}
-
-void Widget::on_hSlider_Side_valueChanged(int value)
-{
-    if(_isAppLoading)
-    {
-        return;
-    }
-
-    qDebug() << Q_FUNC_INFO;
-
-    SysAudio::getInstance().setPartVolume("Side", value);
-    ui->lbl_VolSide->setText(QString::number(value));
+    SysAudio::getInstance().setPartVolume(channelName, volume);
 }
 
 Widget::~Widget()
